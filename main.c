befb520237af835c67d4802613114574aca52550
Vedang Naik <vedang.a.naik@gmail.com>
2020-08-04 11:36:39 +0530

    Interface bind workinggit add .

--- main.c
+++ main.c
@@ -19,7 +19,25 @@ void main() {
         return;
     }
 
-    // no binding, so it'll listen to everything
+    // set interface name
+    const char* ifrName = "lo";
+    // get interface index using ioctl
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(struct ifreq));
+    strncpy(ifr.ifr_name, ifrName, IF_NAMESIZE);
+    if (ioctl(socketHandle, SIOCGIFINDEX, &ifr) == -1) {
+        perror("interface index find failed");
+        return;
+    }
+    // bind socket to interface using sockaddr_ll
+    struct sockaddr_ll sll;
+    sll.sll_family = AF_PACKET;
+    sll.sll_ifindex = ifr.ifr_ifindex;
+    sll.sll_protocol = htons(ETH_P_IP);
+    if (bind(socketHandle, (struct sockaddr*)&sll, sizeof(sll)) == -1) {
+        perror("interface bind failed");
+        return;
+    };
     
     // listen for packets:
     uint8_t buffer[2048];
